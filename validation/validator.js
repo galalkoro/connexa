125995fa6c502fcd209a9b922be0a04b56310fa8
galal <koro-galal@outlook.com>
2022-05-04 12:38:53 +0200

    fixed bg-colr js-file

--- validation/validator.js
+++ validation/validator.js
@@ -6,21 +6,21 @@ if (window.location.href.indexOf('http') !== -1) {
       const validateHTML = document.createElement('button');
       // how would you label this button with ARIA?
       validateHTML.innerHTML = 'validate HTML';
-      validateHTML.style = 'color:black;';
+      validateHTML.style = 'color:black; border-radius: 21px; padding: 0.8em; background-color: green';
       validateHTML.onclick = () => window.open('https://validator.w3.org/nu/?doc=' + encodeURIComponent(window.location.href), '_blank');
       validateContainer.appendChild(validateHTML);
   
       const validateCSS = document.createElement('button');
       // how would you label this button with ARIA?
       validateCSS.innerHTML = 'validate CSS';
-      validateCSS.style = 'color:black;';
+      validateCSS.style = 'color:black; border-radius: 21px; padding: 0.8em; background-color: blue';
       validateCSS.onclick = () => window.open('https://jigsaw.w3.org/css-validator/validator?uri=' + encodeURIComponent(window.location.href), '_blank');
       validateContainer.appendChild(validateCSS);
   
       const developmentStrategyButton = document.createElement('button');
       // how would you label this button with ARIA?
       developmentStrategyButton.innerHTML = 'development strategy';
-      developmentStrategyButton.style = 'color:black;';
+      developmentStrategyButton.style = 'color:black; border-radius: 21px; padding: 0.8em; background-color: red';
       const developmentStrategyA = document.createElement('a');
       developmentStrategyA.href = './development-strategy';
       developmentStrategyA.target = '_blank';
